b25759e265df928d298a1c3efb1aa552b1b59117
Corentin Martens <corentin.martens@hotmail.be>
2015-11-24 17:10:58 +0100

    Work on MeshFilesReader

---
+++ src/MeshFilesReader.cpp
@@ -0,0 +1,26 @@
+#include "MeshFilesReader.h"
+
+MeshFilesReader::MeshFilesReader() {
+
+}
+
+MeshFilesReader::~MeshFilesReader() {
+
+
+}
+
+void MeshFilesReader::getNormals() {
+
+}
+
+void MeshFilesReader::getUVs() {
+
+}
+
+void MeshFilesReader::getVertices() {
+
+}
+
+void MeshFilesReader::read(const char* path) {
+
+}
--- src/MeshFilesReader.h
+++ src/MeshFilesReader.h
@@ -8,6 +8,15 @@ public:
 	MeshFilesReader();
 	~MeshFilesReader();
 
+	void read(const char* path);
+	void getVertices();
+	void getUVs();
+	void getNormals();
+
 private:
+
 	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> uvs;
+	std::vector<glm::vec3> normals;
+
 };
