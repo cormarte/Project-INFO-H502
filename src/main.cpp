5e6ac280d09a3e4dae3795bfee8553b21471cae4
Corentin Martens <corentin.martens@hotmail.be>
2015-11-27 14:28:26 +0100

    Add glut lib. Modify lib paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <gl\glew.h>
 #include <gl\gl.h>
+#include <glut.h>
 #include <iostream>
 #include "MeshFileReader.h"
 using namespace std;
@@ -10,9 +11,22 @@ int main(int argc, char *argv[]) {
 
 	vector<vec3> vertices;
 
-	MeshFileReader::getInstance()->readFile("D:\\Documents de Corentin\\Documents\\ULB\\MA2\\INFO-H-502\\Projet\\data\\baby_original_triangles.ply", vertices);
-	
+	MeshFileReader::getInstance()->readFile("..\\data\\baby_original_triangles.ply", vertices);
+
 	//glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	//init();
+	//glutDisplayFunc(display);
+	//glutReshapeFunc(reshape);
+	//glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	
+	
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -30,4 +44,5 @@ int main(int argc, char *argv[]) {
 	glEnd();
 
 	while (1);
+	return 0;
 }
