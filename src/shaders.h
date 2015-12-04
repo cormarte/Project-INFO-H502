b1848a72fe66b5a6474f18b71dd23f2d0280a8e4
Madli Bayot <mbayot@ulb.ac.be>
2015-12-04 15:35:54 +0100

    Add shaders

---
+++ src/shaders.h
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <glew.h>
+#include <glut.h>
+
+#ifndef __INCLUDESHADERS 
+#define __INCLUDESHADERS 
+
+std::string textFileRead (const char * filename) ;
+void programerrors (const GLint program) ;
+void shadererrors (const GLint shader) ;
+GLuint initshaders (GLenum type, const char * filename) ;
+GLuint initprogram (GLuint vertexshader, GLuint fragmentshader) ;
+
+#endif 
